93453ce75b5c1638fcbf25f7302d0169b7301b06
dli <zq.david.li@gmail.com>
2024-10-07 09:05:37 -0400

    set up skeleton of a page

---
+++ src/app/timeline/page.tsx
@@ -0,0 +1,43 @@
+const TimelineEntries = [
+  {
+    key: 'graduation',
+    description: 'Graduated from the University of Waterloo (Computer Engineering, BASc)',
+    time: 'Jun \'18',
+    image: '',
+  },
+  {
+    key: 'job1',
+    description: 'Joined Flybits as a Junior frontend developer',
+    time: 'Oct \'18',
+    image: '',
+  },
+  {
+    key: 'gap',
+    description: 'Taught English as an Assistant Language Teacher in Nagasaki',
+    time: 'Aug \'19',
+    image: '',
+  },
+  {
+    key: 'job2',
+    description: 'Re-joined Flybits as an intermediate frontend engineer',
+    time: 'Oct \'21',
+    image: '',
+  },
+  {
+    key: 'job3',
+    description: 'Promoted to senior frontend engineer @Flybits',
+    time: 'Apr \'23',
+    image: '',
+  },
+];
+
+const Timeline = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
