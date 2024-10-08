7f7ecc64d4f1a1922a0f9fea3f0c65da64db6f32
dli <zq.david.li@gmail.com>
2024-10-08 09:25:03 -0400

    add timeline

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,40 +1,49 @@
+import './timeline.css';
+
 const TimelineEntries = [
   {
     key: 'graduation',
     description: 'Graduated from the University of Waterloo (Computer Engineering, BASc)',
-    time: 'Jun \'18',
+    label: 'Jun \'18',
     image: '',
   },
   {
     key: 'job1',
     description: 'Joined Flybits as a Junior frontend developer',
-    time: 'Oct \'18',
+    label: 'Oct \'18',
     image: '',
   },
   {
     key: 'gap',
     description: 'Taught English as an Assistant Language Teacher in Nagasaki',
-    time: 'Aug \'19',
+    label: 'Aug \'19',
     image: '',
   },
   {
     key: 'job2',
     description: 'Re-joined Flybits as an intermediate frontend engineer',
-    time: 'Oct \'21',
+    label: 'Oct \'21',
     image: '',
   },
   {
     key: 'job3',
-    description: 'Promoted to senior frontend engineer @Flybits',
-    time: 'Apr \'23',
+    description: 'Promoted to senior frontend engineer at Flybits',
+    label: 'Apr \'23',
     image: '',
   },
 ];
 
+const MAIN_CLASS = 'timeline';
 const Timeline = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex items-center justify-center flex-col min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
+        {TimelineEntries.map(item => (
+          <div key={`timeline-item-${item.key}`} className="flex items-center gap-16">
+            <div className="w-14">{item.label}</div>
+            <div className={`${MAIN_CLASS}__item relative pl-8 border-l-4 border-[var(--foreground)] py-8`}>{item.description}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
