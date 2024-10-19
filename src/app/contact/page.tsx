2c3726fde93948c703137845a10060991b74c80a
dli <zq.david.li@gmail.com>
2024-10-19 13:18:13 -0400

    add flip effect to contact card

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ import './contact.css';
 const MAIN_CLASS = 'contact';
 
 const CardFront = () => (
-  <div className={`${MAIN_CLASS}__card__front w-full h-full flex items-center justify-center`}>
+  <div className={`${MAIN_CLASS}__card__front`}>
     <Image src={Logo} alt="logo" />
   </div>
 );
@@ -25,7 +25,7 @@ const Contact = () => {
   return (
     <div className="flex items-center justify-center flex-col min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div
-        className={`${MAIN_CLASS}__card ${isCardFlipped ? '--flipped' : ''}`}
+        className={`${MAIN_CLASS}__card`}
         role="button"
         onClick={() => setCardFlipped(prev => !prev)}
         tabIndex={0}
@@ -35,7 +35,10 @@ const Contact = () => {
           }
         }}
       >
-        {isCardFlipped ? <CardBack /> : <CardFront />}
+        <div className={`${MAIN_CLASS}__card__inner ${isCardFlipped ? `${MAIN_CLASS}__card__inner--flipped` : ''}`}>
+          <CardBack />
+          <CardFront />
+        </div>
       </div>
     </div>
   );
