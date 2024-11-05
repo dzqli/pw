3338f39d14fb8787f7e27e098b494b9b292caede
dli <zq.david.li@gmail.com>
2024-11-05 09:35:15 -0500

    add simple graphic to intro section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,27 @@
+import Image from "next/image";
+import AboutGraphic from '@/assets/about-graphic-transparent.png';
+
 const MAIN_CLASS = 'about';
 
 const About = () => {
   return (
-    <div className={MAIN_CLASS}>
+    <div className={`${MAIN_CLASS} items-center justify-items-center px-8 gap-16 sm:px-20 font-[family-name:var(--font-geist-sans)]` }>
       <article>
-        <section>
-          <h3>Intro</h3>
-          <p>
-            Hi.<br />
-            I'm David, and I do software engineering!<br/>
-            This site is the beginnings of a personal playground: a place where I
-            get to explore tools, languages, and frameworks I'm interested in - and build utilities I need.<br/>
-            Feel free to poke around, and see what you can find.
-          </p>
+        <section id="intro" className={`${MAIN_CLASS} min-h-screen`}>
+          <h1 className="text-4xl font-bold leading-7 pt-[20vh] mb-2 sm:truncate sm:text-4xl sm:tracking-tight">
+            Hi.  I&#39;m David.
+          </h1>
+          <div className="flex flex-row flex-wrap">
+            <div className="min-w-[300px] w-1/2">
+              <p>
+                I do software engineering!<br/>
+                This site is the beginnings of a personal playground: a place where I
+                get to explore tools, languages, and frameworks I'm interested in - and build utilities I need.<br/>
+              </p>
+
+            </div>
+            <Image className="ml-auto max-w-[200px]" src={AboutGraphic} alt="intro graphic" />
+          </div>
         </section>
         <section>
           <h3>Technologies</h3>
