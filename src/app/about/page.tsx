72f33c5c9ab9113b4c3554d60bcae3c0182d4b59
dli <zq.david.li@gmail.com>
2024-11-10 14:24:51 -0500

    determine shape of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,35 +24,57 @@ const About = () => {
           </div>
         </section>
         <section>
-          <h3>Technologies</h3>
-          <p>
-            With 4+ years of experience building scalable front-end web applications,
-            my expertise primarily lies within <b>React/Javascript</b>.<br/>
-            But my curiosity has led me to get my hands dirty with many other technologies:
-            <ul>
-              <li>Built a script to organize a tangled mess of mp3 using Python</li>
-              <li>Helped out the back-end team by working on Golang microservices</li>
-              <li>Spun up a new service on a Kubernetes cluster to assist with a live demo</li>
-            </ul>
-            To name a few.<br />
-            I'm a strong believer that any software engineering skills are transferable 
-            to all other parts of the stack - so all we have to do is grease those
-            elbows and get building.
-          </p>
+          <h3>5+ years of building scalable web applications</h3>
+          <div>
+            <div>
+              <h4>Frontend Mastery</h4>
+              <p>
+                Drop me in any ecosystem and I'll hit the ground running.  POJO,
+                jQuery, Ember, or Vue?  No sweat, I've seen it all, and more too.
+              </p>
+              <h6>My bread and butter</h6>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>Sass</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h4>Fullstack Shenanigans</h4>
+              <p>
+                Who says devs have to stay in their own part of the stack?
+              </p>
+              <h6>A few of my sidequests</h6>
+              <ul>
+                <li>Untagling a mess of unnamed mp3s by writing a Python script</li>
+                <li>Implementing RESTful endpoints in Golang microservices</li>
+                <li>Spinning up a new service on a Kubernetes cluster for an internal demo</li>
+                <li>Writing Verilog to create a USB3.0 bulk transfer module for FPGAs</li>
+              </ul>
+            </div>
+          </div>
         </section>
         <section>
           <h3>What's next</h3>
           <p>
-            I've always been a big fan of understanding systems as a whole, knowledge
-            with which can inform better decisions made around every individual component.<br/>
-            To that end, I've developed an interest in systems engineering and
-            cloud infrastructure, and am actively seeking any opportunities to work with ops.<br/>
-            Of course, who can forget the headliner of 2024 - generative AI.<br/>
-            There may be a few experiments in that area popping up on this site soon...
+            Every developer needs new toys to play with from time to time.
+            <br />
+            Here are a few of mine:
           </p>
+          <div>
+            <div>k8s</div>
+            <div>ML</div>
+            <div>Tailwind</div>
+            <div>Next.js</div>
+            <div>MongoDB</div>
+            <div>GraphQL</div>
+          </div>
         </section>
         <section>
-          <h3>Other (?)</h3>
           <p>
             Want to get to know me a bit better?
             Enter a certain code created by Hashimoto Kazuhisa ...
