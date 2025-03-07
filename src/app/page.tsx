e1f3b5129ef877a58cf2baa0bf8aa7082bb35bb7
dli <zq.david.li@gmail.com>
2025-03-06 23:16:54 -0500

    center images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,24 @@
 'use client'
 import { getTimeBand } from '@/helpers';
-import Image from 'next/image';
-import dlcSL from '@/assets/dalian.svg';
-import ngsSL from '@/assets/nagasaki.svg';
-import yyzSL from '@/assets/toronto.svg';
+import ngsSL from '@/assets/nagasaki';
+import yyzSL from '@/assets/toronto';
 
 const getImageSource = (timeBand: string) => {
   switch (timeBand) {
     case 'evening':
-      return yyzSL;
+      return { Skyline: yyzSL, height: 'h-1/2' };
     case 'morning':
-      return dlcSL;
+      return { Skyline: ngsSL, height: 'h-1/4' };
     case 'afternoon':
-      return ngsSL;
+      return { Skyline: ngsSL, height: 'h-1/4' };
     default:
-      return ngsSL;
+      return { Skyline: yyzSL, height: 'h-1/2' };
   }
 }
 
 export default function Home() {
   const timeBand = getTimeBand(new Date());
-  const skyline = getImageSource(timeBand);
+  const { Skyline, height } = getImageSource(timeBand);
   return (
     <div className="items-center justify-items-center min-h-[90vh] p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center">
@@ -28,11 +26,7 @@ export default function Home() {
           Good {timeBand}
         </h1>
       </main>
-      <Image
-        className="fixed left-0 bottom-0 h-2/5 object-cover"
-        src={skyline}
-        alt="skyline"
-      />
+        <Skyline className={`fixed left-1/2 -translate-x-1/2 bottom-0 w-auto ${height}`} />
     </div>
   );
 }
