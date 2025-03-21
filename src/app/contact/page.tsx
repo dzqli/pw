f287fc02b3082cbe8a33a78e626b25c39b1609fc
dli <zq.david.li@gmail.com>
2025-03-20 23:00:52 -0400

    fix lint issues

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,7 @@
 'use client'
-import { useState } from 'react';
-import Image from "next/image";
 import ContactCard from '@/components/ContactCard/ContactCard';
-import Logo from '@/assets/sample-logo-transparent.png';
-import LinkedInIcon from '@/assets/icons/linkedin';
-import MailIcon from '@/assets/icons/mail';
 
 const Contact = () => {
-  const [isCardFlipped, setCardFlipped] = useState(false);
-
   return (
     <div className="flex items-center justify-center flex-col min-h-[85vh] p-5 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p className="text-sm text-[var(--secondary)] ml-2 animate-pulse">Tap to flip &darr;</p>
